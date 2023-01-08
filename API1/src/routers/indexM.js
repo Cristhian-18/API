c11f6c85c1702e555aff936b2268e4d811fcf605
Cristhian-18 <77641170+Cristhian-18@users.noreply.github.com>
2023-01-08 02:02:38 -0500

    Delete API1 directory

--- API1/src/routers/indexM.js
+++
@@ -1,9 +0,0 @@
-const {Router}= require('express');
-const router = Router();
-
-
-const { getMarcas }=require('../controllers/index.controlles');
-
-router.get('/',getMarcas);
-
-module.exports=router;
